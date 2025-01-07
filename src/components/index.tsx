645d5008a4e8d5baa6af162072dbead4f2411cd7
Tonito75 <antoinedutonito75@gmail.com>
2025-01-07 17:21:50 +0100

    Typo

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,6 +3,6 @@ export {Dashboard as Tonito_Dashboard} from './dashboard/MainDashboard';
 export type 
 {
     DrawerButton as Tonito_Dashboard_DrawerButton, 
-    DrawerHr as Tonito_Dashboard_Hr,
-    DrawerTypo as Tonito_Dashboard_Typo
+    DrawerHr as Tonito_Dashboard_DrawerHr,
+    DrawerTypo as Tonito_Dashboard_DrawerTypo
 } from './dashboard/dashboardTypes.tsx';
